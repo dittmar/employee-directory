4cff3f32d05daf171abe930e753db73ac1ec8873
dittmar <kevinhd4@gmail.com>
2022-02-21 14:41:49 -0500

    UI, launch image, and app images

---
+++ Employee Directory/Employee List View/EmployeeListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeeListViewController.swift
+//  Employee Directory
+//
+//  Created by Kevin Dittmar on 2/13/22.
+//
+
+import UIKit
+
+final class EmployeeListViewController: UIViewController {
+
+}
+
---
+++ Employee Directory/Employee List View/EmployeeTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EmployeeTableViewCell.swift
+//  Employee Directory
+//
+//  Created by Kevin Dittmar on 2/21/22.
+//
+
+import UIKit
+
+final class EmployeeTableViewCell: UITableViewCell {
+    
+}
